a367380a88098522046ed1dff77ab54dc2060dfa
waelayoub <waelayoub2002@gmail.com>
2023-09-22 12:04:44 +0300

    fixing requirements in python

--- test_flaskApp.py
+++ test_flaskApp.py
@@ -1,11 +1,11 @@
 import unittest
-import requests
+from flask_app import app
 
 
 class TestingFlask(unittest.TestCase):
 
     def test_hello_endpoint(self):
-        response = requests.get("http://127.0.0.1:12121/hello")
+        response = app.test_client().get("/hello")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.text, "HELLO")
 
