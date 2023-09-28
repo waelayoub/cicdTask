537606e4a1c6e31af8df30712733294f54a06a8c
waelayoub <waelayoub2002@gmail.com>
2023-09-28 18:28:53 +0300

    Changing some files

--- test_flaskApp.py
+++ test_flaskApp.py
@@ -1,11 +1,14 @@
 import unittest
+import requests
 from flask_app import app
 
 
 class TestingFlask(unittest.TestCase):
 
     def test_hello_endpoint(self):
-        response = app.test_client().get("/hello")
+        response = requests.get("http://127.0.0.1:12121/hello")
+        # response = app.test_client().get("/hello")
+        # print(response)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.text, "HELLO")
 
